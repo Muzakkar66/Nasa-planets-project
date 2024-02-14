7a9272b2a601bbba25b1ad732daebbff67e2d999
muhammad-muzakkir.asad@ceative.co.uk <muhammad-muzakkir.asad@ceative.co.uk>
2024-02-14 21:32:29 +0500

    read file of kepler is succesfully with fs createReadStream funtion

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const parse = require('csv-parse')
+const { parse } = require("csv-parse");
+const fs = require("fs");
+// parse();
+
+const result = [];
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+    columns: true,
+    })
+  )
+  .on("data", (data) => {
+    result.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(result);
+    console.log("All Done");
+  });
